531626d93a180c1a4d18dec43cb8a8337989676c
ysooj <musukie52@gmail.com>
2025-08-14 21:44:29 +0900

    (2025.8.14) 거품 정렬

---
+++ Program/Class5th (Insert Sort)/Source.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// ========================================================
+// 
+// !!!!!!!!!!!!!!!!!!!!!다시 보기 필수!!!!!!!!!!!!!!!!!!!!!
+// 
+//              코드 똑같은 지 다시 확인하기
+// 
+// ========================================================
+
+int main()
+{
+#pragma region 삽입 정렬
+	// 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
+	// 자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘입니다.
+
+	int list[] = { 7, 6, 8, 4, 1, 3, 10, 5, 2, 9 };
+
+	int size = sizeof(list) / sizeof(list[0]);
+
+	int key = 0;
+	int j = 0;
+
+	for (int i = 1; i < size; i++)
+	{
+		key = list[i];
+
+		for (int j = i - 1; j >= 0 && list[j] > key; j--)
+		{
+			list[j + 1] = list[j];
+		}
+
+		list[j + 1] = key;
+	}
+
+	cout << endl;
+	cout << "정렬된 배열 : ";
+	for (const auto & element : list)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -2,11 +2,89 @@
 
 using namespace std;
 
+// 1. 두 개의 자연수 x, y가 있을 때
+//    x와 y를 나눈 나머지를 r이라고 합니다.
+
+// 2. r이 0이라면, x와 y의 최대공약수가 y가 됩니다.
+
+// 3. r이 0이 아니라면, x와 y의 최대 공약수는 y와 r의 최대 공약수와 같기 때문에
+//    x에는 y 그리고 y에는 r을 대입한 후 1번으로 돌아가면 됩니다.
+
+int Max(int x, int y)
+{
+	int r = x % y;
+
+	if (r == 0)
+	{
+		return y;
+	}
+
+	return Max(y, r);
+}
+
 int main()
 {
-#pragma region MyRegion
-
+#pragma region 유클리드 호제법 - 내 코드
+	//	// 2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로
+	//	// 두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
+	//	
+	//	int num1 = 24;
+	//	int num2 = 36;
+	//	
+	//	int max = 1;
+	//	
+	//	if (num1 > num2)
+	//	{
+	//		for (int i = 1; i < num1; i++)
+	//		{
+	//			if (num1 % i == 0 && num2 % i == 0)
+	//			{
+	//				max = i;
+	//			}
+	//		}
+	//	}
+	//	
+	//	else
+	//	{
+	//		for (int i = 1; i < num2; i++)
+	//		{
+	//			if (num1 % i == 0 && num2 % i == 0)
+	//			{
+	//				max = i;
+	//			}
+	//		}
+	//	}
+	//	
+	//	cout << max << endl;
 #pragma endregion
+
+#pragma region 유클리드 호제법 - 선생님 코드
+	//	int x = 72;
+	//	int y = 109;
+	//	
+	//	int result = 0;
+	//	
+	//	for (int i = 1; i <= x && i <= y; i++)
+	//	{
+	//		if (x % i == 0 && y % i == 0)
+	//		{
+	//			result = i;
+	//		}
+	//	}
+	//	
+	//	cout << "최대 공약수 : " << result << endl;
+#pragma endregion
+
+#pragma region 유클리드 호제법 - 최종
+	int x = 24;
+	int y = 36;
+
+	int result = Max(x, y);
+
+	cout << result << endl;
+#pragma endregion
+
+
 
 	return 0;
 }
