836d350cbdb4f6e2329f1c1d74d0492a973e65b8
ysooj <musukie52@gmail.com>
2025-08-27 21:43:57 +0900

    (2025.8.27) 탐욕법

---
+++ Program/Class15th (Depth First Search)/Source.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+template <typename T>
+
+class Graph
+{
+private:
+	unordered_set<T> visited;
+	unordered_map<T, vector<T>> adjacencyList;
+public:
+	void insert(const T& i, const T& j)
+	{
+		adjacencyList[i].push_back(j);
+		adjacencyList[j].push_back(i);
+	}
+
+	void search(const T& start)
+	{
+		if (visited.count(start))
+		{
+			return;
+		}
+
+		visited.insert(start);
+
+		cout << start << " ";
+
+		for (const auto& element : adjacencyList[start])
+		{
+			if (visited.count(element) == false)
+			{
+				search(element);
+			}
+		}
+	}
+};
+
+int main()
+{
+#pragma region 깊이 우선 탐색 (Depth First Search)
+	// 그래프에서 한 방향으로 갈 수 있을 만큼 깊이 들어갔다가
+	// 더 이상 갈 수 없으면 다시 돌아와서 다른 경로를 탐색하는 방법입니다.
+
+	Graph <char> graph;
+
+	graph.insert('A', 'B');
+	graph.insert('A', 'C');
+	graph.insert('B', 'D');
+	graph.insert('B', 'E');
+	graph.insert('C', 'F');
+	graph.insert('F', 'G');
+
+	graph.search('A');
+#pragma endregion
+
+	return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,19 +1,18 @@
 ﻿#include <iostream>
+#include <unordered_map>
 #include <vector>
+#include <queue>
 
 using namespace std;
+
+template <typename T>
 
 class Graph
 {
 private:
-	vector<bool> visited;
-	vector<vector<char>> adjacencyList;
+	unordered_map<T, vector<T>> adjacencyList;
 public:
-	Graph(int size)
-	{
-		adjacencyList.resize(abs(size));
-	}
-	void insert(int i, int j)
+	void insert(const T & i, const T & j)
 	{
 		adjacencyList[i].push_back(j);
 		adjacencyList[j].push_back(i);
@@ -22,18 +21,18 @@ public:
 
 int main()
 {
-#pragma region 깊이 우선 탐색 (Depth First Search)
-	// 그래프에서 한 방향으로 갈 수 있을 만큼 깊이 들어갔다가
-	// 더 이상 갈 수 없으면 다시 돌아와서 다른 경로를 탐색하는 방법입니다.
+#pragma region 너비 우선 탐색 (Breadth First Search)
+	// 하나의 시작 정점을 방문한 후, 시작 정점에 인접한 모든 정점들을 우선적으로 방문하는 탐색입니다.
 
-	Graph graph(100);
+	Graph <char> graph;
 
 	graph.insert('A', 'B');
 	graph.insert('A', 'C');
 	graph.insert('B', 'D');
 	graph.insert('B', 'E');
 	graph.insert('C', 'F');
-	graph.insert('F', 'G');
+	graph.insert('C', 'G');
+
 #pragma endregion
 
 	return 0;
