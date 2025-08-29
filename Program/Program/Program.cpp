90c1b6dc15d71b54ae4510e536d97daca170fef6
ysooj <musukie52@gmail.com>
2025-08-29 21:45:07 +0900

    (2025.8.29) 너비 우선 탐색

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,6 +1,72 @@
 ﻿#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
 
 using namespace std;
+
+template <typename T>
+
+class Graph
+{
+private:
+	unordered_map <T, vector<T>> adjacencyList;
+	unordered_map <T, int> degree;
+	unordered_set <T> vertices;
+public:
+
+	void insert(const T & i, const T & j)
+	{
+		adjacencyList[i].push_back(j);
+		degree[j]++;
+
+		vertices.insert(i);
+		vertices.insert(j);
+
+		if (degree.count(i) == false)
+		{
+			degree[i] = 0;
+		}
+	}
+
+	void search()
+	{
+		queue <T> queue;
+		vector <T> forPrint;
+
+		for (const auto& element : vertices)
+		{
+			if (degree[element] == 0)
+			{
+				queue.push(element);
+			}
+		}
+
+		while (queue.empty() == false)
+		{
+			T x = queue.front();
+
+			queue.pop();
+
+			forPrint.push_back(x);
+
+			for (const T & element : adjacencyList[x])
+			{
+				degree[element]--;
+
+				if (degree[element] == 0)
+				{
+					queue.push(element);
+				}
+			}
+		}
+
+		for (const T & element : forPrint)
+		{
+			cout << element << " ";
+		}
+	}
+};
 
 int main()
 {
@@ -23,6 +89,23 @@ int main()
 
 	// 진입 차수 : in-degree. 한 정점으로 들어오는 간선의 개수 → 해당 노드를 의존하는 노드 수
 	// 진출 차수 : out-degree. 한 정점에서 나가는 간선의 개수 → 해당 노드가 의존하는 다른 노드 수
+
+	Graph <int> graph;
+
+	graph.insert(1, 2);
+	graph.insert(1, 5);
+
+	graph.insert(2, 3);
+
+	graph.insert(3, 4);
+
+	graph.insert(4, 6);
+
+	graph.insert(5, 6);
+
+	graph.insert(6, 7);
+
+	graph.search();
 #pragma endregion
 
 	return 0;
